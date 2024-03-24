59aa64501c2fb60d9b117de920df32f6a380f5a4
pi-josh <joshua.macatunao007@gmail.com>
2024-03-24 23:46:14 +0800

    Assigned parts for each group member to work on

--- 3. ΩMG/Ohms.java
+++ 3. ΩMG/Ohms.java
@@ -8,7 +8,7 @@
 
 // Note: Functions name and return type are subject to change later
 
-public class Omega {
+public class Ohms {
 	public static void main(String[] args) {
 		System.out.println("Testing");
 	}
