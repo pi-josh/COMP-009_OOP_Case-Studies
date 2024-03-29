5a21bb6aac5cc20867f1b79ac8d1672268e6d089
pi-josh <joshua.macatunao007@gmail.com>
2024-03-29 17:34:44 +0800

    Added TODO tasks for each member to work on

--- 3. ΩMG/Ohms.java
+++ 3. ΩMG/Ohms.java
@@ -19,10 +19,94 @@ public class Ohms {
 	*/
 	static Scanner in = new Scanner(System.in);
 
+	/**
+	 * This is the main method which run the application
+	 */
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		// Run the program repeatedly until the user chooses to exit
+		System.out.println("++++++++++++++++++++ OHMS ++++++++++++++++++++");
+		char choice = 'y';
+		do {
+			// Getting the colors of the resistor's three bands
+			String[] threeBands = getThreeBands();
+
+			// Getting the resistance color value of the resistor's three bands
+			int resistanceValue = calculateResistanceValue(threeBands, threeBands.length);
+
+			// Displaying the result
+			System.out.printf("Resistance value: %d kilo-ohms\n\n", resistanceValue);
+
+			// Keep prompting the user if they still want to decode until they enter a valid input
+			while(true) {
+				System.out.println("Do you want to decode another resistor?");
+				System.out.print("=> ");
+				choice = in.next().charAt(0);
+				choice = Character.toLowerCase(choice);		// To make sure that the input will be in lowercase regardless
+
+				if(choice == 'y' || choice == 'n') {
+					break;
+				} else {
+					System.out.println("Please enter 'y' or 'n' only!\n");
+				}
+			}
+		} while(choice != 'n');
+		System.out.println("++++++++++++++++++++ THANK YOU ++++++++++++++++++++");
 	}
 
+	// Dom
+	/**
+	 * This method is used to get the colors of the resistor's three bands.
+	 * The user will be prompted to input the color of each making sure that it is not in CAPS.
+	 * It will then call the searchBand method to 
+	 * 
+	 * @return String[] This return an array of strings that represent the color the resistor's three bands
+	 */
+	public static String[] getThreeBands() {
+		// List of color codes
+		final String[] COLOR_CODES = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white"};
 
-	// Will still discuss the functions to be used...
+		// TODO: Prompt the user to input the colors of the three bands
+
+		// TODO: Check if each color inputted exist in the available color codes
+
+		// TODO: Return the array of the colors of the resistor's three bands here
+		return new String[] {"this", "is", "temporary"};
+	}
+
+	// Cas
+	/**
+	 * This method is used as a helper function to check
+	 * if the band inputted by the user exist in the list.
+	 * The method will return the subscript of the element that matches the target,
+	 * otherwise returns -1.
+	 * 
+	 * @param COLOR_CODES This is an array of strings representing the available colors and the first parameter for calculateResistanceValue method
+	 * @param size This represent the size of the array bandsList
+	 * @param target This represent the target band that will be searched through the list
+	 * 
+	 * @return int This return an integer value that will determine if the target band is in the list.
+	 */
+	public static int searchBand(String[] COLOR_CODES, int size, int target) {
+		// TODO: Check if the target band is in the string of color codes (you can use a for loop here)
+		// then return the subscript of the color code if it matches the target, otherwise return -1
+
+		return -1;
+	}
+
+	// Raf
+	/**
+	 * This method is used to calculate the resistance value of the resistor's three bands.
+	 * 
+	 * @param bandsList This is an array of strings representing the color of the resistor's three bands and the first parameter for calculateResistanceValue method
+	 * @param size This represent the size of the array bandsListand the second parameter for calculateResistanceValue method
+	 * 
+	 * @return int This return an integer value representing the resistance value of the three resistor's three bands.
+	 */
+	public static int calculateResistanceValue(String[] bandsList, int size) {
+		// TODO: Calculate here (you can comment/explain on how you come up with the solution in solving it)
+
+		// TODO: Return the value here
+		return -1; // temporary lang
+	}
+
 }
