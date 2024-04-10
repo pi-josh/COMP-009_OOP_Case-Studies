441f863fd24c152f6b22b20e84c112bdf8877786
pi-josh <joshua.macatunao007@gmail.com>
2024-04-10 13:25:32 +0800

    Progress(MyDate.java): Added implementation and documentation for the setDate method

--- Case Study 2/MyDate.java
+++ Case Study 2/MyDate.java
@@ -12,25 +12,36 @@
  */
 
 public class MyDate {
-	// What is this?
+	// Declaration of private instance variables
 	private int year;
 	private int month;
 	private int day;
 
-	// What is this?
+	// Declaration and initialization of array of constant variables
 	public static final String MONTHS[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
 										   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 	public static final String DAYS[] = {"Sunday", "Monday", "Tuesday", "Wednesday",
 									     "Thursday", "Friday", "Saturday"};
 	public static final int DAYS_IN_MONTHS[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+	/**
+	 * A class constructor that will set the date
+	 */ 
 	@constructor
 	public MyDate(year, month, day) {
-		setDate(year, month, day);
+		setDate(year, month, day);	// Calling setDate method here
 	}
 
 
-	// What is this?
+	/**
+	 * This method is used to set the date once validated by the isValidDate method.
+	 * Otherwise, it will throw an exception.
+	 * 
+	 * @param year  Represents the year of the date.
+	 * @param month Represents the month of the year.
+	 * @param day 	Represents the day of the month.
+	 * @throws IllegalArgumentException If one of the parameter is invalidated
+	 */
 	public void setDate(int year, int month, int day) {
 		try {
 			if(isValidDate()) {
